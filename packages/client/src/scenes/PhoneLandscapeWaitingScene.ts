aba0566ef96aa12a43d22f4281acf50d70e32fe3
xitios660-ctrl <xitios660@gmail.com>
2026-09-24 22:13:31 -0300

    Keep soundtrack alive in phone waiting screen

--- packages/client/src/scenes/PhoneLandscapeWaitingScene.ts
+++ packages/client/src/scenes/PhoneLandscapeWaitingScene.ts
@@ -21,8 +21,6 @@ export class PhoneLandscapeWaitingScene extends Phaser.Scene {
 
   create(data?: { round?: RoundState }): void {
     audio.setPhoneLite(true);
-    audio.stopBgm(0);
-    audio.stopAmbient(0);
     this.bg = this.add.graphics().setDepth(0);
     this.frame = this.add.graphics().setDepth(2);
 
